4059bf4b892f39c8468039d82afe4fdceb19106c
Shai <lfking@gmail.com>
2015-10-09 00:22:08 +0300

    Two things: 1. Fixed the %static problem 2. Added the shopping-list page - its is empty now

--- Order/urls.py
+++ Order/urls.py
@@ -7,6 +7,6 @@ tasks.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'ezorder.views.get_index'),
+    url(r'^$', 'ezorder.views.get_index', name="index"),
     url(r'^ezorder/', include(ezorder.urls)),
 ]
--- ezorder/views.py
+++ ezorder/views.py
@@ -5,9 +5,19 @@ from ezorder import tasks
 
 @require_GET
 def get_index(request):
-    return render_to_response('index.html')
+    return render_to_response('index.html',
+                              {
+        'selected_nav': 'index',
+    })
 
-@require_POST
+@require_GET
+def shopping_list(request):
+    return render_to_response('shopping_list.html',
+                              {
+        'selected_nav': 'shop',
+    })
+
+#@require_POST
 def order(request):
     channel = os.urandom(16).encode('hex')
     t = tasks.track_order(channel)
