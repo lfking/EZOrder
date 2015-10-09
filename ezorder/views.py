4ac02333a7ad2609663494cffa4c94a6b8860d94
Shai <lfking@gmail.com>
2015-10-09 03:28:36 +0300

    Shopping list first working draft Plus some minor fixes to the alignment Plus scripts rearrange

--- ezorder/views.py
+++ ezorder/views.py
@@ -1,4 +1,5 @@
 import os
+import json
 from django.shortcuts import render, render_to_response
 from django.views.decorators.http import require_GET, require_POST
 from ezorder import tasks
@@ -12,9 +13,34 @@ def get_index(request):
 
 @require_GET
 def shopping_list(request):
+    items = [
+  {
+    'selected': True,
+    'brands': [
+         {'name': '1L Olivia olive oil', 'quantity': 2, 'weight_unit': None},
+         {'name': '2L Tnuva olive oil', 'quantity': 1, 'weight_unit': None},
+    ],
+  },
+  {
+    'selected': True,
+    'brands': [
+         {'name': 'Tomatos', 'quantity': 2, 'unit_weight': 0.5, 'weight_unit': 'kg'},
+         {'name': 'Organic Tomatos', 'quantity': 5, 'unit_weight': 0.2, 'weight_unit': 'kg'}
+    ],
+  },
+  {
+    'selected': False,
+    'brands': [
+         {'name': '500g Kosher Salt', 'quantity': 1, 'weight_unit': None},
+         {'name': '2L Tnuva olive oil', 'quantity': 1, 'weight_unit': None}
+    ],
+  }
+]
+    data = json.dumps(items)
     return render_to_response('shopping_list.html',
                               {
         'selected_nav': 'shop',
+        'items': data,
     })
 
 #@require_POST
